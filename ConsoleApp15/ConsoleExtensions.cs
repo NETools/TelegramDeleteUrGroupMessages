676be3c9c2554b1d4604d0881b0ce75e7b049bea
NETools <enes.hergul215@gmail.com>
2023-02-19 15:09:38 +0100

    Projektdateien hinzufügen.

---
+++ ConsoleApp15/ConsoleExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp15
+{
+    internal static class ConsoleExtensions
+    {
+        public static int ReadLineInt32(string promptMessage)
+        {
+            Console.Write(promptMessage);
+            var input = Console.ReadLine();
+            int result = 0;
+            if (input != null)
+                result = int.Parse(input);
+            return result;
+        }
+
+        public static string ReadLineString(string promptMessage)
+        {
+            Console.Write(promptMessage);
+            var input = Console.ReadLine();
+            return input;
+        }
+
+    }
+}
---
+++ ConsoleApp15/Program.cs
@@ -0,0 +1,86 @@
+﻿using ConsoleApp15;
+using TL;
+
+int api_code = ConsoleExtensions.ReadLineInt32("Type in api_id: ");
+string api_hash = ConsoleExtensions.ReadLineString("Type in api_hash: ");
+
+WTelegram.Client client = new WTelegram.Client(api_code, api_hash);
+
+string telNo = ConsoleExtensions.ReadLineString("Type in telphone number (+00xxxxxxxx): ");
+
+await DoLogin(telNo);
+
+async Task DoLogin(string loginInfo)
+{
+    while (client.User == null)
+        switch (await client.Login(loginInfo))
+        {
+            case "verification_code": loginInfo = ConsoleExtensions.ReadLineString("Type in verification code (see app): "); break;
+            case "name": loginInfo = ConsoleExtensions.ReadLineString("Type in name: "); break;
+            case "password": loginInfo = ConsoleExtensions.ReadLineString("Type in password: "); break;
+            default: loginInfo = null; break;
+        }
+    Console.WriteLine($"We are logged-in as {client.User} (id {client.User.id})");
+}
+
+Console.ForegroundColor = ConsoleColor.Red;
+Console.Write("ONLY PRESS ENTER WHEN YOU WANT TO REMOVE YOUR HISTORY FROM TELEGRAM!!!");
+Console.ForegroundColor = ConsoleColor.Gray;
+
+Console.ReadLine();
+
+var chats = await client.Messages_GetAllChats();
+
+Console.ForegroundColor = ConsoleColor.Red;
+Console.WriteLine("You are registered in following groups: ");
+Console.ForegroundColor = ConsoleColor.Gray;
+
+foreach (var (id, chat) in chats.chats)
+{
+    switch (chat)
+    {
+        case Channel group when group.IsGroup && !group.IsChannel:
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"{id}: Group {group.username}: {group.title}");
+            Console.ForegroundColor = ConsoleColor.Gray;
+
+            int offset = 0;
+            bool done = false;
+            while (!done)
+            {
+                var messages = await client.Messages_GetHistory(group.ToInputPeer(), offset);
+                Console.WriteLine($"Searching batch {i}");
+                var userMessages = messages.Messages.Where(p => p.From != null && p.From.ID == client.User.id)
+                    .Select(p => (p.ID, p.ToString()));
+
+                var deleteResult = await client.DeleteMessages(group.ToInputPeer(), userMessages.Select(p => p.ID).ToArray());
+                userMessages.Select(p => p.Item2).ToList().ForEach(message =>
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine($"DELETED: {message}");
+                    Console.ForegroundColor = ConsoleColor.Gray;
+
+                    if (message != null && message.Contains("[ChatAddUser]"))
+                        done = true;
+                });
+
+                offset = messages.Messages[messages.Messages.Length - 1].ID;
+
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.WriteLine($"Waiting 1 s..");
+                Console.ForegroundColor = ConsoleColor.Gray;
+                Thread.Sleep(1000);
+
+            }
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"ALL YOUR MESSAGES DELETED FROM {group.title}");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            break;
+    }
+}
+
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine($"DONE! YOU NEVER EXISTED ON TELEGRAM!");
+Console.ForegroundColor = ConsoleColor.Gray;
